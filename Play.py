d8426fa333cbfa10a205c71cb675e3ff93f94783
Wilson <Wilopez95@gmail.com>
2020-03-07 19:19:31 -0600

    Show the pieces on the screem

--- Play.py
+++ Play.py
@@ -8,12 +8,112 @@ clock =  pygame.time.Clock()
 
 Board = Board()
 Board.createBoard('pta1')
-Board.printBoard()
+#Board.printBoard()
+
+allTiles = []
+allPieces = []
+currentPlayer = Board.currentPlayer
+
+
+def createSqParams():
+    allSqRanges = []
+    xMin = 0
+    xMax = 100
+    yMin = 0
+    yMax = 100
+    for _ in range(8):
+        for _ in range(8):
+            allSqRanges.append([xMin, xMax, yMin, yMax])
+            xMin += 100
+            xMax += 100
+        xMin = 0
+        xMax = 100
+        yMin += 100
+        yMax += 100
+    return allSqRanges
+
+def squares(x,y,w,h,color):
+    pygame.draw.rect(gameDisplay,color,[x,y,w,h])
+    allTiles.append([color,[x,y,w,h]])
+
+def drawChessPieces():
+    xpos = 0
+    ypos = 0
+    color = 0
+    width = 100
+    height = 100
+    black = (0,0,0)
+    white = (255, 255, 255)
+    blue = (0, 0, 128)
+    red = (200, 0, 0)
+    purple = (102, 0, 102)
+    number = 0
+    for _ in range(8):
+        for _ in range(8):
+            if color % 2 == 0:
+                squares(xpos, ypos, width, height, blue)
+                if not Board.gameTiles[number].pieceOnTile.toString() == "-":
+                    img = pygame.image.load(
+                        "./ChessArt/" + Board.gameTiles[number].pieceOnTile.alliance[0].upper() +
+                        Board.gameTiles[
+                            number].pieceOnTile.toString().upper() + ".png")
+                    img = pygame.transform.scale(img, (100, 100))
+                    allPieces.append([img, [xpos, ypos], Board.gameTiles[number].pieceOnTile])
+                xpos += 100
+            else:
+                squares(xpos, ypos, width, height, red)
+                if not Board.gameTiles[number].pieceOnTile.toString() == "-":
+                    img = pygame.image.load(
+                        "./ChessArt/" + Board.gameTiles[number].pieceOnTile.alliance[0].upper() +
+                        Board.gameTiles[
+                            number].pieceOnTile.toString().upper() + ".png")
+                    img = pygame.transform.scale(img, (100, 100))
+                    allPieces.append([img, [xpos, ypos], Board.gameTiles[number].pieceOnTile])
+                xpos += 100
+
+            color += 1
+            number += 1
+        color += 1
+        xpos = 0
+        ypos += 100
+
+
+def updateChessPieces():
+
+    xpos = 0
+    ypos = 0
+    number = 0
+    newPieces = []
+
+    for _ in range(8):
+        for _ in range(8):
+            if not Board.gameTiles[number].pieceOnTile.toString() == "-":
+
+                img = pygame.image.load(
+                    "./ChessArt/" + Board.gameTiles[number].pieceOnTile.alliance[0].upper() + Board.gameTiles[
+                        number].pieceOnTile.toString().upper() + ".png")
+                img = pygame.transform.scale(img, (100, 100))
+
+                newPieces.append([img, [xpos, ypos], Board.gameTiles[number].pieceOnTile])
+            xpos += 100
+            number += 1
+        xpos = 0
+        ypos += 100
+
+    return newPieces
+
+
+allSqParams = createSqParams()
+drawChessPieces()
 
 
 
-##Ciclo del juego , mientras quitGame sea falso la ventana se mantiene abierta##
+selectedImage = None
+selectedLegals = None
+resetColors = []
 quitGame = False
+mx, my = pygame.mouse.get_pos()
+prevx, prevy = [0,0]
 
 while not quitGame:
 
@@ -22,6 +122,17 @@ while not quitGame:
             quitGame = True
             pygame.quit()
             quit()
+
+
+
+
+    gameDisplay.fill((255, 255, 255))
+
+    for info in allTiles:
+        pygame.draw.rect(gameDisplay, info[0], info[1])
+
+    for img in allPieces:
+        gameDisplay.blit(img[0], img[1])
 
     pygame.display.update()
     clock.tick(60)
--- board/Board.py
+++ board/Board.py
@@ -1,17 +1,20 @@
 from board.tile import Tile
 from pieces.nullPiece import NullPiece
-from pieces import bishop
-from pieces import king
-from pieces import knight
-from pieces import pawn
-from pieces import queen
-from pieces import rook
+from pieces.bishop import Bishop
+from pieces.king import King
+from pieces.knight import Knight
+from pieces.pawn import Pawn
+from pieces.queen import Queen
+from pieces.rook import Rook
 
 
 
 class Board:
 
+    ##Lista que almacena todas las piezas del tablero
     gameTiles = {}
+    ##Jugador que inicia jugando
+    currentPlayer = None
 
     def __init__(self):
         pass
@@ -26,39 +29,42 @@ class Board:
                 break
             if line[0] != '#':
                 if line != '\n':
-                    for x in range(2,18):
-                        if(line[x] != '|'):
-                            count += 1
-                            ##print(count,line[x])
-                            self.loadPieces(count,line[x])
+                    if line[0] == 'W' or line[0] == 'B' :
+                        self.currentPlayer = line
+                    else:
+                        for x in range(2,18):
+                            if(line[x] != '|'):
+                                count += 1
+                                #print(count,line[x])
+                                self.loadPieces(count,line[x])
 
         file1.close()
 
     def loadPieces(self,n,p):
         if(p=='K'):
-            self.gameTiles[n] = Tile(n, king.King("Black", n))
+            self.gameTiles[n] = Tile(n, King("Black", n))
         elif(p=='k'):
-            self.gameTiles[n] = Tile(n, king.King("White", n))
+            self.gameTiles[n] = Tile(n, King("White", n))
         elif(p=='Q'):
-            self.gameTiles[n] = Tile(n, queen.Queen("Black", n))
+            self.gameTiles[n] = Tile(n, Queen("Black", n))
         elif(p=='q'):
-            self.gameTiles[n] = Tile(n, queen.Queen("White", n))
+            self.gameTiles[n] = Tile(n, Queen("White", n))
         elif(p=='N'):
-            self.gameTiles[n] = Tile(n, knight.Knight("Black", n))
+            self.gameTiles[n] = Tile(n, Knight("Black", n))
         elif(p=='n'):
-            self.gameTiles[n] = Tile(n, knight.Knight("White", n))
+            self.gameTiles[n] = Tile(n, Knight("White", n))
         elif(p=='B'):
-            self.gameTiles[n] = Tile(n, bishop.Bishop("Black", n))
+            self.gameTiles[n] = Tile(n, Bishop("Black", n))
         elif(p=='b'):
-            self.gameTiles[n] = Tile(n, bishop.Bishop("White", n))
+            self.gameTiles[n] = Tile(n, Bishop("White", n))
         elif(p=='R'):
-            self.gameTiles[n] = Tile(n, rook.Rook("Black", n))
+            self.gameTiles[n] = Tile(n, Rook("Black", n))
         elif(p=='r'):
-            self.gameTiles[n] = Tile(n, rook.Rook("White", n))
+            self.gameTiles[n] = Tile(n, Rook("White", n))
         elif(p=='P'):
-            self.gameTiles[n] = Tile(n, pawn.Pawn("Black", n))
+            self.gameTiles[n] = Tile(n, Pawn("Black", n))
         elif(p=='p'):
-            self.gameTiles[n] = Tile(n, pawn.Pawn("White", n))
+            self.gameTiles[n] = Tile(n, Pawn("White", n))
         else:
             self.gameTiles[n] = Tile(n,NullPiece())
 
